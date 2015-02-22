2b97a5b5fbb30ae7b01651e4a1d971b39e6aab87
Dmitriy Savchenko <login.was.here@gmail.com>
2015-02-22 14:46:27 +0200

    Keep adding add tack form

--- app/pods/components/add-track-form/component.js
+++ app/pods/components/add-track-form/component.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  title: null,
+  artist:null,
+  actions: {
+    save: function(){
+      let formValues = this.getProperties('title', 'artist');
+      this.sendAction('onSave', formValues);
+    }
+  }
 });
